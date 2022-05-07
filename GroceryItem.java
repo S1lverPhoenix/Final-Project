8bf114f2e632a7b203ba33fc93cb085770234f01
Anika Parkhi <arparkhi@icloud.com>
2022-05-07 10:03:45 -0700

    j

--- GroceryItem.java
+++ GroceryItem.java
@@ -5,9 +5,20 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class GroceryItem {
-    private Image myItem;
+    private Image image;
     private String name;
     private int price;
-    
+    private boolean show;
+    private int xLoc;
+    private int yLoc;
+
+    public GroceryItem(Image i, String s){
+        image = i;
+        name = s;
+    }
+
+    public String getName(){
+        return name;
+    }
     
 }
