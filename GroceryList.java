fcc6a5255af5b650dca8b735c3460f028c7c7cd6
Anika Parkhi <arparkhi@icloud.com>
2022-05-07 10:09:26 -0700

    h

--- GroceryList.java
+++ GroceryList.java
@@ -2,12 +2,18 @@ import java.io.File;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.imageio.ImageIO;
 
 public class GroceryList {
     private static Image items;
+	private static final int SQUARE_HEIGHT = 100;
+	private static final int SQUARE_WIDTH = 100;
+    private static ArrayList<GroceryItem> groceryItems;
+	private static String[] names = {"Pineapples", "Bread", "Yogurt", "Strawberries", "Peanut Butter", "Pudding", "Tuna", "Cookies", "Pretzels", "Potatoes", "Macaroni", "Apples", "Null", "Peanuts", "Milk", "Cheese", "Chips", "Cereal", "Broccoli", "Eggplant", "Carrots", "Eggs", "Hot Dogs", "Banana", "Muffins"}
     
-    private static void openItems() {
+	private static void openImage() {
 		if(items==null) {
 			try {
 				File f = new File("images/grocery.jpeg");
@@ -18,4 +24,19 @@ public class GroceryList {
 		
         }
     }
+	protected static Image openImageFromSpriteSheet(int x, int y, int w, int h) {
+		openImage();
+		return ((BufferedImage)items).getSubimage(x,y,w,h).getScaledInstance(SQUARE_WIDTH, SQUARE_HEIGHT, BufferedImage.SCALE_SMOOTH);
+	}
+	public void makeGroceryList(){
+		int widthCount = SQUARE_WIDTH;
+		int heightCount = SQUARE_HEIGHT;
+		for(int x = 0; x<names.length; x++){
+			groceryItems.add(new GroceryItem(openImageFromSpriteSheet(widthCount, heightCount, SQUARE_WIDTH, SQUARE_HEIGHT), names[x]));
+			if(groceryItems.get(x).getName().equals("Null")){
+				groceryItems.remove(x);
+		}
+		
+		}
+	}
 }
