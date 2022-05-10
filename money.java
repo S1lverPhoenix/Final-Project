a8536dcea5af6d2066b3f33708206816dd025042
Anika Parkhi <arparkhi@icloud.com>
2022-05-10 11:01:41 -0700

    j

--- money.java
+++ money.java
@@ -1,3 +1,11 @@
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 public class Money{
-    
+    private Image bill;
+    private int amount;
+
 }
