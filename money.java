d2e4fa659c485ea874e1313e3773dc0201d8ebf2
sruthiaravind <99357747+sruthiaravind@users.noreply.github.com>
2022-05-11 11:11:42 -0700

    money

--- money.java
+++ money.java
@@ -1,15 +1,37 @@
-import javax.imageio.ImageIO;
-import java.io.File;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.imageio.*;
+import javax.swing.ImageIcon;
 
-public class Money {
+public class Money extends JFrame implements ActionListener {
+    JFrame frame;
+    JButton[] money = new JButton[6];
+    JButton oneButton, fiveButton, tenButton, twentyButton, fiftyButton, hundredButton;
+    JPanel j;
+
     private Image bill;
     private int value;
 
-    public Money(Image bill, int value) {
-        
+    public Money() {
+        frame=new JFrame("Money");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1260,550);
+        frame.setLayout(null);
+        //setting dollar bills to buttons
+        Icon one = new ImageIcon("images/money/one.jpeg");
+        oneButton= new JButton(one);
+        Icon five = new ImageIcon("images/money/five.jpeg");
+        fiveButton= new JButton(five);
+        Icon ten = new ImageIcon("images/money/ten.jpeg");
+        tenButton= new JButton(ten);
+        Icon twenty = new ImageIcon("images/money/twenty.jpeg");
+        twentyButton= new JButton(twenty);
+        Icon fifty = new ImageIcon("images/money/fifty.jpeg");
+        fiftyButton= new JButton(fifty);
+        Icon hundred = new ImageIcon("images/money/one.jpeg");
+        hundredButton= new JButton(hundred);
+
     }
 
     public int getValue() {
@@ -19,5 +41,8 @@ public class Money {
     public void draw (Graphics g) {
 
     }
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
+    } 
 }
