7f49b98753a3c082879c8a17a65905432ffa4463
Anika Parkhi <arparkhi@icloud.com>
2022-05-12 09:12:08 -0700

    k

--- GroceryItem.java
+++ GroceryItem.java
@@ -6,12 +6,14 @@ import javax.imageio.ImageIO;
 import java.net.URL;
 
 public class GroceryItem {
-    private Image image;
+    private Image items;
     private String name;
     private double price;
     private boolean show;
     private int xLoc;
     private int yLoc;
+    private static final int SQUARE_HEIGHT = 75;
+	private static final int SQUARE_WIDTH = 100;
 	private static String[][] names = {{"Pineapples", "Bread", "Yogurt", "Strawberries", "Peanut Butter"}, {"Pudding", "Tuna", "Cookies", "Pretzels", "Potatoes"}, {"Macaroni", "Apples", "Null", "Peanuts", "Milk"},{ "Cheese", "Chips", "Cereal", "Broccoli", "Eggplant"}, {"Carrots", "Eggs", "Hot Dogs", "Banana", "Muffins"}};
 
     public GroceryItem(String s){
@@ -31,20 +33,38 @@ public class GroceryItem {
     //     java.awt.image.BufferedImage img = ImageIO.read(url);
     // }
 
-    public void draw(){
-
-        for(int x = 0; x<names.length; x++){
-            for(int y = 0; y<names[0].length; y++){
-                if(name.equals(names[x][y])){
-                    
-                }
-            }
-        }
-    }
+    
 
     public Image getImage(){
         return image;
     }
     
+    private static void openImage() {
+		if(items==null) {
+			try {
+				File f = new File("images/grocery.jpeg");
+				items = ImageIO.read(f);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		
+        }
+    }
+	protected static Image openImageFromSpriteSheet(int x, int y, int w, int h) {
+		openImage();
+		return ((BufferedImage)items).getSubimage(x,y,w,h).getScaledInstance(SQUARE_WIDTH, SQUARE_HEIGHT, BufferedImage.SCALE_SMOOTH);
+	}
+
+    public void draw(){
+
+        for(int x = 0; x<names.length; x++){
+            for(int y = 0; y<names[0].length; y++){
+                if(name.equals(names[x][y])){
+                    openImageFromSpriteSheet(x*SQUARE_HEIGHT, y*SQUARE_WIDTH, SQUARE_WIDTH, SQUARE_HEIGHT);
+                    
+                }
+            }
+        }
+    }
 }
 
--- GroceryList.java
+++ GroceryList.java
@@ -7,9 +7,6 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 public class GroceryList {
-    private static Image items;
-	private static final int SQUARE_HEIGHT = 100;
-	private static final int SQUARE_WIDTH = 100;
     private static ArrayList<GroceryItem> groceryItems;
 	private static String[][] names = {{"Pineapples", "Bread", "Yogurt", "Strawberries", "Peanut Butter"}, {"Pudding", "Tuna", "Cookies", "Pretzels", "Potatoes"}, {"Macaroni", "Apples", "Null", "Peanuts", "Milk"},{ "Cheese", "Chips", "Cereal", "Broccoli", "Eggplant"}, {"Carrots", "Eggs", "Hot Dogs", "Banana", "Muffins"}};
     
@@ -17,21 +14,6 @@ public class GroceryList {
 		return groceryItems;
 	}
 
-	private static void openImage() {
-		if(items==null) {
-			try {
-				File f = new File("images/grocery.jpeg");
-				items = ImageIO.read(f);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		
-        }
-    }
-	protected static Image openImageFromSpriteSheet(int x, int y, int w, int h) {
-		openImage();
-		return ((BufferedImage)items).getSubimage(x,y,w,h).getScaledInstance(SQUARE_WIDTH, SQUARE_HEIGHT, BufferedImage.SCALE_SMOOTH);
-	}
 	public void makeGroceryList(){
 		int count = 0;
 		for(int x = 0; x<names.length; x++){
