45878f93054418943104be710e11d4f408a9afea
br0wnHuman <99357989+br0wnHuman@users.noreply.github.com>
2022-05-17 10:30:09 -0700

    notes on cart (for me)

--- Cart.java
+++ Cart.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.JTextField;
 
 public class Cart{
     
@@ -8,3 +9,6 @@ public class Cart{
 
     }
 }
+//for every button(name, price) under the item of food, 
+//there will be a textbox containing (name, count). the shelf is a 5 by 5 containing 24 items 
+//so at the bottom of each column there will be 5 textbozes for each of the items
